c35edcfbdf17a3def5b4df48b19e46db1beecba0
Yasin Eryılmaz <yasin.eryilmaz@metus.com>
2024-09-15 11:13:00 +0300

    Card added

--- src/pages/Admin/HomePage.tsx
+++ src/pages/Admin/HomePage.tsx
@@ -1,13 +1,35 @@
-// pages/Admin/HomePage.js
-import React from "react";
+import React from 'react';
 
-const HomePage = () => {
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
+import Paper from '@mui/material/Paper';
+import { experimentalStyled as styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
+
+export default function ResponsiveGrid() {
   return (
-    <div>
-      <h1>Ana Sayfa</h1>
-      <p>Bu, yönetim panelinin ana sayfasıdır.</p>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 2 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {Array.from(Array(100)).map((_, index) => (
+          <Grid key={index} size={{ xs: 2, sm: 4, md: 3, lg: 2 }}>
+            <Item>{index + 1}</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
-};
-
-export default HomePage;
+}
