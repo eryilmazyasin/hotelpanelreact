0dacbee79ac9e0035ce1e5c3e7091c6eef53da87
Yasin Eryılmaz <yasin.eryilmaz@metus.com>
2024-09-15 13:36:36 +0300

    Room card added

--- src/pages/Admin/HomePage.tsx
+++ src/pages/Admin/HomePage.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 
+import BedroomParentIcon from '@mui/icons-material/BedroomParent';
+import CheckIcon from '@mui/icons-material/Check';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid2';
 import Paper from '@mui/material/Paper';
-import { experimentalStyled as styled } from '@mui/material/styles';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
-}));
+import './Homepage.scss';
 
 export default function ResponsiveGrid() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} className="homepage-wrapper">
       <Grid
         container
         spacing={{ xs: 2, md: 2 }}
@@ -26,7 +18,32 @@ export default function ResponsiveGrid() {
       >
         {Array.from(Array(100)).map((_, index) => (
           <Grid key={index} size={{ xs: 2, sm: 4, md: 3, lg: 2 }}>
-            <Item>{index + 1}</Item>
+            <div className="card">
+              <div className="card-title">
+                <BedroomParentIcon />
+                <span>Oda {index + 1}</span>
+              </div>
+              <div className="card-body">
+                <span>Deluxe Oda</span>
+                {/* //rezarvasyon yapılmış ise bu blok aktif olacak */}
+                {/* <div className="reservation-info">
+                  <span className="reservation-name">
+                    <CheckIcon /> Yasin Eryılmaz
+                  </span>
+                  <span>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  </span>
+                </div> */}
+              </div>
+
+              {/* //rezarvasyon yapılmış ise bu blok aktif olacak */}
+              {/* <div className="date">
+                <span>15.09.2021</span>
+                <br />
+                <span>20.09.2021</span>
+              </div> */}
+              <Button variant="contained">Rezervasyon Yap</Button>
+            </div>
           </Grid>
         ))}
       </Grid>
